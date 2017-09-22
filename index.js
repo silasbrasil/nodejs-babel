ffab0a7b63ab6f1863d48c25d81bd6096e85d4ca
silasbrasil <silas.swf@gmail.com>
2017-09-22 16:54:53 -0300

    Cria algumas rotas para testar com POSTMAN

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-import http from 'http';
+import express from 'express';
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(9090, 'localhost');
+const app = express();
+let requestCounter = 0;
 
-console.log('Server running at http://localhost:9090/');
+app.get('/', (req, res) => {
+  requestCounter++;
+  res.send('[GET][/]');
+  console.log('REQUEST: ', requestCounter);
+});
+
+app.get('/home', (req, res) => {
+  requestCounter++;
+  res.send('[GET][/home]');
+  console.log('REQUEST: ', requestCounter);
+});
+
+app.get('/curso', (req, res) => {
+  requestCounter++;
+  res.send('[GET][/curso]');
+  console.log('REQUEST: ', requestCounter);
+});
+
+app.listen(9090, () => {
+  console.log('Server running at http://localhost:9090/');
+});
